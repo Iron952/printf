58b4a4ce035817b4cacfc75c8e864e368163d7a1
Philani <89873186+Philani12@users.noreply.github.com>
2022-10-19 05:53:57 +0200

    Added a few prototypes

--- main.h
+++ main.h
@@ -23,19 +23,25 @@ typedef struct call
 	int (*b)(char *, va_list, int);
 } call_a;
 
+int int_func(char *buff_dest, va_list list, int buff_count);
 int printf_pointer(va_list val);
 int printf_hex_aux(unsigned long int num);
 int printf_HEX_aux(unsigned int num);
 int printf_exclusive_string(va_list val);
 int printf_HEX(va_list val);
 int printf_hex(va_list val);
+int hexidec_func(char *buff_dest, va_list arg, int buff_count);
 int printf_oct(va_list val);
+int octal_func(char *buff_dest, va_list arg, int buff_count);
 int printf_unsigned(va_list args);
 int printf_bin(va_list val);
+int binary_func(char *buff_dest, va_list arg, int buff_count);
 int printf_srev(va_list args);
 int printf_rot13(va_list args);
+int sign_func(char *buff_dest, va_list arg, int buff_count);
 int printf_int(va_list args);
 int printf_dec(va_list args);
+int other_func(char *buff_dest, va_list arg, int buff_count);
 int _strlen(char *s);
 int *_strcpy(char *dest, char *src);
 int _strlenc(const char *s);
@@ -45,6 +51,7 @@ int printf_37(void);
 int printf_char(va_list val);
 int printf_string(va_list val);
 int _putchar(char c);
+int unint_func(char *buff_dest, va_list arg, int buff_count);
 int _printf(const char *format, ...);
 
 #endif
